8276faaf208ca2ed0fd722330c2421b1176d73bd
98yejin <o0o2211915@gmail.com>
2025-04-09 10:00:44 -0400

    refactor: update import statements to use 'import type' for ReactNode

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { useSidebar } from "@/context/SidebarContext";
