2de426f4eaa21768875ad9516093bc7320094c57
oneofthezombies <hunhoekim@gmail.com>
2025-04-09 22:41:30 -0400

    feat: add TRPC UI panel API endpoint for development environment

---
+++ src/pages/api/panel.ts
@@ -0,0 +1,16 @@
+import { appRouter } from "@/server/routers/_app";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  if (process.env.NODE_ENV !== "development") {
+    return res.status(404).send("Not Found");
+  }
+
+  const { renderTrpcPanel } = await import("trpc-ui");
+  res.status(200).send(
+    renderTrpcPanel(appRouter, {
+      url: "/api/trpc",
+      transformer: "superjson",
+    })
+  );
+}
