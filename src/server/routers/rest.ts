e4d3be7b59ef21f86f7372dd4d1acd7319e23e95
oneofthezombies <hunhoekim@gmail.com>
2025-04-15 01:48:20 +0000

    style: format code for better readability in rest.ts

--- src/server/routers/rest.ts
+++ src/server/routers/rest.ts
@@ -1,16 +1,22 @@
+import type constants from "constants";
 import { Hono } from "hono";
 import { envConfig } from "../env-config";
 import { trpcRouter } from "./trpc";
 
-export const rest = new Hono().basePath("/api").get("/trpc-ui", async (c) => {
-  if (envConfig.nodeEnv !== "development") {
-    return c.text("Not Found", 404);
-  }
-  const { renderTrpcPanel } = await import("trpc-ui");
-  return c.html(
-    renderTrpcPanel(trpcRouter, {
-      url: "/api/trpc",
-      transformer: "superjson",
-    })
-  );
-});
+export const rest = new Hono()
+  .basePath("/api")
+  .get("/trpc-ui", async (c) => {
+    if (envConfig.nodeEnv !== "development") {
+      return c.text("Not Found", 404);
+    }
+    const { renderTrpcPanel } = await import("trpc-ui");
+    return c.html(
+      renderTrpcPanel(trpcRouter, {
+        url: "/api/trpc",
+        transformer: "superjson",
+      })
+    );
+  })
+  .post("/chats/:id/start", async (c) => {
+    const { id } = c.req.param();
+  });
