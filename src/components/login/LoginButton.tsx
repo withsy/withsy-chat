1692c3e13be2c04b437769d3b94aa262a711871a
98yejin <o0o2211915@gmail.com>
2025-04-16 17:12:30 +0000

    feat: enhance user authentication flow with logout option and improve login button styling

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -1,19 +1,19 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+import { useUser } from "@/context/UserContext";
+import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user?.email ?? "Unknown email"} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+  const { userPrefs } = useUser();
+  // Signed in as {session.user?.email ?? "Unknown email"}
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <Button
+      size="sm"
+      onClick={() => signIn()}
+      style={{
+        backgroundColor: `rgb(${userPrefs.themeColor})`,
+      }}
+    >
+      Log in
+    </Button>
   );
 }
