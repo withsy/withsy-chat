39893d3bd5f88609e3271ce745e673c7d859bd1c
98yejin <o0o2211915@gmail.com>
2025-04-18 22:19:59 +0000

    fix: update z-index value in Header component for improved layering

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ import { HeaderTooltipGroup } from "./HeaderTooltip";
 export default function Header() {
   const { data: session } = useSession();
   return (
-    <header className="flex items-center justify-between h-16 px-4 py-1 z-150">
+    <header className="flex items-center justify-between h-16 px-4 py-1 z-40">
       <HeaderTooltipGroup />
       <div className="flex items-center ml-auto gap-4">
         {!session ? <LoginButton /> : <UserDropdownMenu />}
