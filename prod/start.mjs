68abd24b5c0b6d4478488306b719945ffd69193a
oneofthezombies <hunhoekim@gmail.com>
2025-04-19 21:59:18 +0000

    fix: refactor environment variable handling in start.mjs for clarity

--- prod/start.mjs
+++ prod/start.mjs
@@ -2,18 +2,23 @@ import { execSync } from "node:child_process";
 
 console.log("SECRET_MANAGER_ENV start");
 console.log(process.env.SECRET_MANAGER_ENV);
-console.log("SECRET_MANAGER_ENV done");
+console.log("SECRET_MANAGER_ENV end");
 
 const command = process.argv.slice(2).join(" ");
 console.log("command:", command);
 
+const env = {
+  ...process.env,
+  ...Object.fromEntries(
+    process.env.SECRET_MANAGER_ENV.split("\n").map((x) => x.split("="))
+  ),
+};
+console.log("env start");
+console.log(env);
+console.log("env end");
+
 execSync(command, {
   shell: true,
   stdio: "inherit",
-  env: {
-    ...process.env,
-    ...Object.fromEntries(
-      process.env.SECRET_MANAGER_ENV.split("\n").map((x) => x.split("="))
-    ),
-  },
+  env,
 });
