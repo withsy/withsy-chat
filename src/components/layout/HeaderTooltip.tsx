14ac31c68d6a1b72e0c5e798251319a7bc2ad538
98yejin <o0o2211915@gmail.com>
2025-04-20 21:57:05 +0000

    feat: enhance touch interaction for HeaderTooltip buttons

--- src/components/layout/HeaderTooltip.tsx
+++ src/components/layout/HeaderTooltip.tsx
@@ -7,9 +7,12 @@ import {
 } from "@radix-ui/react-tooltip";
 import { PanelRightClose, PanelRightOpen, SquarePen } from "lucide-react";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export function HeaderTooltipGroup() {
   const { isMobile, setCollapsed, collapsed, toggle } = useSidebar();
+  const [collapseTouch, setCollapseTouch] = useState(false);
+  const [newChatTouch, setNewChatTouch] = useState(false);
   const router = useRouter();
 
   const iconSize = 20;
@@ -22,12 +25,16 @@ export function HeaderTooltipGroup() {
 
   return (
     <TooltipProvider>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-3">
         <Tooltip>
           <TooltipTrigger asChild>
             <button
               onClick={toggle}
-              className="cursor-pointer rounded-md group w-8 h-8 flex items-center justify-center hover:bg-white"
+              className={`cursor-pointer rounded-md group w-8 h-8 flex items-center justify-center hover:bg-white  ${
+                collapseTouch ? "bg-white" : ""
+              }`}
+              onTouchStart={() => setCollapseTouch(true)}
+              onTouchEnd={() => setCollapseTouch(false)}
             >
               {collapsed ? (
                 <PanelRightClose
@@ -54,7 +61,11 @@ export function HeaderTooltipGroup() {
           <TooltipTrigger asChild>
             <button
               onClick={handleLinkClick}
-              className="cursor-pointer rounded-md group w-8 h-8 flex items-center justify-center hover:bg-white"
+              className={`cursor-pointer rounded-md group w-8 h-8 flex items-center justify-center hover:bg-white ${
+                newChatTouch ? "bg-white" : ""
+              }`}
+              onTouchStart={() => setNewChatTouch(true)}
+              onTouchEnd={() => setNewChatTouch(false)}
             >
               <SquarePen size={iconSize} className="group-hover:text-black" />
             </button>
