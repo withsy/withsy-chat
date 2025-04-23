05803ad1857c4f367965d616cb2689ff0a13dce6
oneofthezombies <hunhoekim@gmail.com>
2025-04-23 15:00:28 -0400

    feat: add ChatPrompt and Prompt models with relationships in schema.prisma and migration.sql

---
+++ prisma/migrations/20250423185932_prompt/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "chat_prompts" (
+    "id" SERIAL NOT NULL,
+    "chat_id" UUID NOT NULL,
+    "parent_prompt_id" UUID,
+    "text" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "chat_prompts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "prompts" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "is_starred" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "prompts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "chat_prompts_chat_id_idx" ON "chat_prompts"("chat_id");
+
+-- CreateIndex
+CREATE INDEX "chat_prompts_parent_prompt_id_idx" ON "chat_prompts"("parent_prompt_id");
+
+-- CreateIndex
+CREATE INDEX "prompts_user_id_idx" ON "prompts"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "chat_prompts" ADD CONSTRAINT "chat_prompts_chat_id_fkey" FOREIGN KEY ("chat_id") REFERENCES "chats"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "chat_prompts" ADD CONSTRAINT "chat_prompts_parent_prompt_id_fkey" FOREIGN KEY ("parent_prompt_id") REFERENCES "prompts"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "prompts" ADD CONSTRAINT "prompts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
