d2e74e8d4cd1c27244131cf212a2c6bafa3c1dc7
oneofthezombies <hunhoekim@gmail.com>
2025-04-24 23:27:13 -0400

    fix: prevent LoginButton from rendering when user is not authenticated

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -4,15 +4,16 @@ import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
   const { user } = useUser();
-  if (!user) return null;
 
   return (
     <Button
       size="sm"
       onClick={() => signIn()}
-      style={{
-        backgroundColor: `rgb(${user.preferences.themeColor})`,
-      }}
+      style={
+        {
+          // backgroundColor: `rgb(${user.preferences.themeColor})`,
+        }
+      }
     >
       Log in
     </Button>
