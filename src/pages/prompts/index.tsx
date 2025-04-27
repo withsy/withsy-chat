6412e44ce16580469a5460da8277900ea3fed64c
98yejin <o0o2211915@gmail.com>
2025-04-27 03:30:02 +0000

    feat: rename BookmarkIndex to PromptsPage and update return structure to render a placeholder div

--- src/pages/prompts/index.tsx
+++ src/pages/prompts/index.tsx
@@ -1,11 +1,10 @@
-import BookmarkPage from "@/components/bookmarks/BookmarkPage";
 import { PartialLoading } from "@/components/Loading";
 import { useUser } from "@/context/UserContext";
 
-export default function BookmarkIndex() {
+export default function PromptsPage() {
   const { user } = useUser();
   if (!user || user == null) {
     return <PartialLoading />;
   }
-  return <BookmarkPage user={user} />;
+  return <div />;
 }
