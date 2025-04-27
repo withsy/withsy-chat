ff7c01476ef3024086abf1de6bfe85e588183143
98yejin <o0o2211915@gmail.com>
2025-04-27 16:16:32 -0400

    fix: update property name from isStar to isStarred in PromptsTable component

--- src/components/prompts/PromptsTable.tsx
+++ src/components/prompts/PromptsTable.tsx
@@ -66,7 +66,7 @@ export function PromptsTable({
                     className="flex items-center justify-center hover:text-yellow-500 transition-colors w-5 h-5"
                     data-ignore-row-click
                   >
-                    {prompt.isStar ? (
+                    {prompt.isStarred ? (
                       <Star
                         className="w-4 h-4 text-black"
                         style={{ fill: `rgb(${themeColor})` }}
