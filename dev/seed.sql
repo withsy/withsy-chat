7ab71a57f8d56ffcc51ae1aeb8fb7d56b00c5930
oneofthezombies <hunhoekim@gmail.com>
2025-05-01 14:26:17 -0400

    fix: adjust user_usage_limits insertion to remove timezone function

--- dev/seed.sql
+++ dev/seed.sql
@@ -7,5 +7,5 @@ INSERT INTO user_link_accounts(user_id, provider, provider_account_id)
   VALUES ('b24c885e-a501-4882-938b-f5fb16cbd395', 'credentials', 'withsy-dev');
 
 INSERT INTO user_usage_limits(user_id, daily_remaining, daily_reset_at, minute_remaining, minute_reset_at) 
-  VALUES ('b24c885e-a501-4882-938b-f5fb16cbd395', 30, date_trunc('day', timezone('UTC', NOW()) + interval '1 day'), 6, timezone('UTC', NOW()) + interval '1 minute');
+  VALUES ('b24c885e-a501-4882-938b-f5fb16cbd395', 30, date_trunc('day', NOW() + interval '1 day'), 6, NOW() + interval '1 minute');
 
