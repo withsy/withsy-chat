e57a76c2c049414b1d4cf0af8147be5420453ca0
98yejin <o0o2211915@gmail.com>
2025-05-05 16:58:21 -0400

    chore : apply tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
