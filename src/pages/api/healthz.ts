817316ba6b68aec0925d67f4f38955a0024b1f7d
oneofthezombies <hunhoekim@gmail.com>
2025-05-07 03:03:27 -0400

    chore: add AWS SDK S3 client dependency and update health check API documentation

--- src/pages/api/healthz.ts
+++ src/pages/api/healthz.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  // NOTE: Since the Render service checks the server status with this endpoint,
+  // you should not change the /api/healthz path.
   return res.status(200).send("ok");
 }
