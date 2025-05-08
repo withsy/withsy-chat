9fdc70308c0361feac8747d44fd5926c8b324787
oneofthezombies <hunhoekim@gmail.com>
2025-05-08 06:54:46 -0400

    feat: add migration worker script for database migrations

---
+++ prod/migrate-worker.ts
@@ -0,0 +1,15 @@
+import { runMigrations } from "graphile-worker";
+import { Pool } from "pg";
+
+main();
+
+async function main() {
+  const pgPool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  await runMigrations({ pgPool });
+}
