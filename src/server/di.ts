7bb81fdbfb7cec71d101ca2e0d5e2a9a1b1bd17c
harryplusplus <harryplusplus@gmail.com>
2025-11-14 01:29:23 +0900

    feat: Add DI Container

---
+++ src/server/di.ts
@@ -0,0 +1,81 @@
+interface Provider<TInjectable> {
+  (): TInjectable;
+}
+
+type AnyProviderMap = Record<string, Provider<any>>;
+
+interface Injector<TProviderMap extends AnyProviderMap> {
+  <TName extends keyof TProviderMap>(name: TName): ReturnType<
+    TProviderMap[TName]
+  >;
+}
+
+export class Container<TProviderMap extends AnyProviderMap> {
+  readonly #injectableMap: Record<string, any> = {};
+  readonly #providerMap: TProviderMap;
+
+  constructor(providerMap: TProviderMap) {
+    this.#providerMap = providerMap;
+  }
+
+  getInjectable<TName extends keyof TProviderMap>(
+    name: TName
+  ): ReturnType<TProviderMap[TName]> {
+    const nameString = name.toString();
+
+    if (nameString in this.#injectableMap) {
+      return this.#injectableMap[nameString];
+    }
+
+    const provider = this.#providerMap[nameString];
+    if (!provider) {
+      throw new Error(`[${nameString}] provider not exists.`);
+    }
+
+    const injectable = provider();
+    if (!injectable) {
+      throw new Error(`[${nameString}] provider is invalid.`);
+    }
+
+    this.#injectableMap[nameString] = injectable;
+    return injectable;
+  }
+
+  getInjector(): Injector<TProviderMap> {
+    return this.getInjectable.bind(this);
+  }
+
+  static newBuilder(this: void): ContainerBuilder<{}> {
+    return new ContainerBuilder({});
+  }
+}
+
+class ContainerBuilder<TProviderMap extends AnyProviderMap> {
+  readonly #providerMap: TProviderMap;
+
+  constructor(providerMap: TProviderMap) {
+    this.#providerMap = providerMap;
+  }
+
+  addProvider<TName extends string, TInjectable>(
+    name: TName,
+    provider: Provider<TInjectable>
+  ): ContainerBuilder<
+    TProviderMap & {
+      [key in TName]: Provider<TInjectable>;
+    }
+  > {
+    if (name in this.#providerMap) {
+      throw new Error(`[${name}] provider exists.`);
+    }
+
+    return new ContainerBuilder({
+      ...this.#providerMap,
+      [name]: provider,
+    });
+  }
+
+  build(): Container<TProviderMap> {
+    return new Container<TProviderMap>(this.#providerMap);
+  }
+}
--- src/server/services/db.ts
+++ src/server/services/db.ts
@@ -1,8 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import type { ServiceRegistry } from "../service-registry";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 export function createDb(_s: ServiceRegistry) {
-  const prisma = new PrismaClient();
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+  const prisma = new PrismaClient({ adapter });
   process.on("SIGTERM", async () => {
     await prisma.$disconnect();
   });
