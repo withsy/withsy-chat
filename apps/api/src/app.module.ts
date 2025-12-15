a7850fdf67261911ca1f2361fcf7b842689ae1b8
harryplusplus <harryplusplus@gmail.com>
2025-12-15 09:48:51 +0900

    wip: api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { GracefulShutdownModule } from "@tygra/nestjs-graceful-shutdown";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GracefulShutdownModule.forRoot()],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { setupGracefulShutdown } from "@tygra/nestjs-graceful-shutdown";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupGracefulShutdown({ app });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
