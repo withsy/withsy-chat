fd40f8c21917a48085ce327fa5331d802b3e147c
harryplusplus <harryplusplus@gmail.com>
2025-12-16 12:56:11 +0900

    wip: api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,13 @@ import { checkTimeZoneUtc } from "./utils";
 async function bootstrap() {
   checkTimeZoneUtc();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ["https://withsy-chat.vercel.app", "http://localhost:3000"],
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization", "X-Api-Key"],
+    },
+  });
   setupGracefulShutdown({ app });
 
   await app.listen(process.env.PORT ?? 3100);
