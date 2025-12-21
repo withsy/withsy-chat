2a56fe676233b639663d312727fb6be212b5824e
harryplusplus <harryplusplus@gmail.com>
2025-12-21 12:48:14 +0900

    fix: Remove staleTime

--- apps/web/src/lib/trpc.ts
+++ apps/web/src/lib/trpc.ts
@@ -20,9 +20,6 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
         refetchOnWindowFocus: false,
         refetchOnReconnect: false,
       },
