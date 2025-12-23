99fee168601fe2ffcd2833ac882e6535bd822d1a
harryplusplus <harryplusplus@gmail.com>
2025-12-23 12:29:08 +0900

    wip: userStore chatMap

--- apps/web/src/stores/useUserStore.ts
+++ apps/web/src/stores/useUserStore.ts
@@ -3,6 +3,7 @@ import { devtools } from "zustand/middleware";
 
 const STORE_NAME = "user";
 
+// TODO: UserContext에서 상태와 훅 분리.
 interface UserStore {}
 
 export const useUserStore = create<UserStore>()(
