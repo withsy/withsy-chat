72f0d4c41c88982c81c65b980f2af25c69441d3d
harryplusplus <harryplusplus@gmail.com>
2025-12-31 23:24:04 +0900

    wip: Saved

--- apps/web/src/components/Error.tsx
+++ apps/web/src/components/Error.tsx
@@ -1,27 +1,23 @@
 import { Activity } from "lucide-react";
 
-type ErrorProps = {
+interface Props {
   message?: string;
-};
+}
 
-export function FullPageError({
-  message = "Something went wrong.",
-}: ErrorProps) {
+export function FullPageError({ message = "Something went wrong." }: Props) {
   return (
     <div className="flex h-[100dvh] w-screen flex-col items-center justify-center space-y-4 text-center">
       <Activity className="h-10 w-10" />
-      <p className="text-muted-foreground">ERROR: {message.toUpperCase()}</p>
+      <p className="text-muted-foreground">ERROR: {message}</p>
     </div>
   );
 }
 
-export function PartialError({
-  message = "Something went wrong.",
-}: ErrorProps) {
+export function PartialError({ message = "Something went wrong." }: Props) {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center space-y-2 text-center">
       <Activity className="h-8 w-8" />
-      <p className="text-muted-foreground">ERROR: {message.toUpperCase()}</p>
+      <p className="text-muted-foreground">ERROR: {message}</p>
     </div>
   );
 }
